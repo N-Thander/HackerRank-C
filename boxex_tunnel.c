4f26addb944cb35280fd5c4c85b4c8335e780c35
N-Thander <108251321+N-Thander@users.noreply.github.com>
2023-07-20 23:09:22 +0530

    Create boxex_tunnel.c

---
+++ boxex_tunnel.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+	/**
+	* Define three fields of type int: length, width and height
+	*/
+    int length;
+    int width;
+    int height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+	/**
+	* Return the volume of the box
+	*/
+    int vol;
+    vol = b.length*b.width*b.height;
+    return vol;
+}
+
+int is_lower_than_max_height(box b) {
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+    if (b.height < 41) {
+    return 1;
+    }
+    else {
+    return 0;
+    }
+}
